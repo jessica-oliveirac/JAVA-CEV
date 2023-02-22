7f8f954376c6d023290dae9e2c49b9c5e165ff91
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-02-21 22:58:45 -0300

    Create Aula.java

---
+++ Aula.java
@@ -0,0 +1,17 @@
+package primeiroprograma;
+public class Primeiro Programa {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
+
+---------------------------------------------------
+import Java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println ("Hora do sistema: ");
+        System.out.println(relogio.toString());
+    }
+}
