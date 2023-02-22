c4a40719367e2fb995ca540c073bdffad37e19b3
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-02-22 02:11:37 -0300

    Update Aula.java

--- Aula.java
+++ Aula.java
@@ -1,17 +1,35 @@
+
+
 package primeiroprograma;
-public class Primeiro Programa {
+public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
     }
 }
-
 ---------------------------------------------------
-import Java.util.Date;
+package horadosistema;
+
+import java.util.Date;
 
 public class HoraDoSistema {
     public static void main(String[] args) {
-        Date relogio = new Date();
-        System.out.println ("Hora do sistema: ");
+        Date relogio = new Date ();
+        System.out.println("Horário do sistema: ");
         System.out.println(relogio.toString());
     }
 }
+
+#IDIOMA DO SISTEMA
+
+package idiomadosistema;
+
+import java.util.Locale;
+public class IdiomaDoSistema {
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.print("Idioma do sistema: ");
+        System.out.print(loc.getDisplayLanguage());
+    }
+}
+---------------------------------------------------
+RESOLUÇÃO DO SISTEMA
