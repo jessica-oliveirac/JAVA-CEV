31df654327d460f7537af4971b733e98c367142b
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-02-23 16:39:09 -0300

    Create 6) Tipos Primitivos.java

---
+++ 6) Tipos Primitivos.java
@@ -0,0 +1,36 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    float nota = 8.5f;
+		System.out.println(" Sua nota é: " + nota);
+	}
+}
+
+-----------------------------------------------
+public class Main
+{
+	public static void main(String[] args) {
+	    float nota = 8.5f;
+		System.out.printf(" Sua nota é %.2f", nota);
+	}
+}
+
+-----------------------------------------------
+public class Main
+{
+	public static void main(String[] args) {
+	    String nome = Jéssica;
+		System.out.println("A nota da %s é %.2.f \n", nome, nota);
+	}
+}
+
+†***************************************
+Entrada
+
+-----------------------------------------------
+
+-----------------------------------------------
+
+
+-----------------------------------------------
+
